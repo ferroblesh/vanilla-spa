06086c78dd77d3df49bae21dcc24c2f5bd818681
Fernando Robles <ferroblesh@gmail.com>
2020-04-13 00:01:09 -0500

    Added Home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+     <div class="Characters">
+        <article class="Character-item">
+            <a href="#/1">
+                <img src="image" alt="image" />
+            </a>
+        </article>
+     </div>
+    `
+    return view;
+};
+
+export default Home;
